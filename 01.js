6487018db5e3528e9f35139e4f57335c6ea95b8b
danwang <1318704976@qq.com>
2024-11-24 17:26:33 +0800

    切片完成啦

--- 01.js
+++
@@ -1,9 +0,0 @@
-// const fs = require('fs')
-// fs.writeFile("./nodeTest.txt", "hello", err => {
-//    if(err) console.log(err)
-// })
-// fs.readFile("nodeTest.txt", (err, data) => {
-//     console.log(data.toString())
-// })
-const path = require('path')
-console.log(path.join(__dirname, 'nodeTest.txt'))
